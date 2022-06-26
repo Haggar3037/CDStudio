4efa65bfdbb4ffbe39d179c4e10b7516cffeca1c
Haggar3037 <83113883+Haggar3037@users.noreply.github.com>
2022-06-27 03:55:48 +0700

    Тестоое задание. Без адаптива

---
+++ js/index.js
@@ -0,0 +1,70 @@
+let topBar = document.querySelector(".top-bar");
+let header = document.querySelector(".header");
+let buttons = document.querySelectorAll(".top-bar__menu__element");
+let linkLists = document.querySelectorAll(".top-bar__menu-link-list")
+
+window.addEventListener('scroll', () => {
+    let scrollDistanse = window.scrollY;
+    let backgroundValue;
+    let activChek = 0;
+
+    if (scrollDistanse >= 100) {
+        topBar.classList.add("top-bar_activ");
+        let backgroundOpacity = (scrollDistanse/500).toFixed(2);
+        backgroundValue = "linear-gradient(rgba(255, 255, 255," + backgroundOpacity + "), rgba(255, 255, 255," + backgroundOpacity + ")), url(../img/header.png)"
+        header.style.backgroundImage = backgroundValue;
+    }
+
+    if (scrollDistanse < 100) {
+        for(i = 0; i < buttons.length; i++) {
+            if(buttons[i].classList.contains("top-bar__menu__element_activ")) {
+                activChek = 1;
+            }
+        }
+        if(activChek == 0) {
+            topBar.classList.remove("top-bar_activ");
+            backgroundValue = "linear-gradient(rgba(255, 255, 255, 0.0), rgba(255, 255, 255,  0.0)), url(../img/header.png)"
+            header.style.backgroundImage = backgroundValue;
+        }
+    }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    let scrollDistanse = window.scrollY;
+    if (scrollDistanse >= 100) {
+        topBar.classList.add("top-bar_activ");
+    }
+});
+
+for (i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", menuEvent);
+}
+
+
+function menuEvent(event) {
+    console.log(event);
+    let button = event.currentTarget; 
+    let scrollDistanse = window.scrollY;
+    let activChek = 0;
+    for (i = 0; i < buttons.length; i++){
+        if (buttons[i].classList.contains("top-bar__menu__element_activ")) {
+            buttons[i].classList.remove("top-bar__menu__element_activ");
+            linkLists[i].classList.remove("top-bar__menu-link-list_activ");
+            if(buttons[i] == button){
+                activChek = 1;
+            }   
+        }
+    }
+    if(activChek == 0) {
+        for (i = 0; i < buttons.length; i++){
+            if(buttons[i] == button){
+                buttons[i].classList.add("top-bar__menu__element_activ");
+                linkLists[i].classList.add("top-bar__menu-link-list_activ");
+            }
+        }
+        topBar.classList.add("top-bar_activ");
+    } else if (scrollDistanse < 100){
+        topBar.classList.remove("top-bar_activ");
+    }
+}
+
---
+++ js/pludins-main.js
@@ -0,0 +1,10 @@
+$('.slider__container').slick({
+    prevArrow: document.getElementById('slider-button-left'),
+    nextArrow: document.getElementById('slider-button-right')
+});
+
+$('.slider-opinion__container').slick({
+    prevArrow: document.getElementById('slider-opinion-button-left'),
+    nextArrow: document.getElementById('slider-opinion-button-right'),
+    variableWidth: true
+});
